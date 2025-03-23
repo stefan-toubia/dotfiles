ff01c1ceabb403458233c10ae767a15364b78705
Stefan Toubia <stefan.toubia@gmail.com>
2025-03-23 10:59:34 -0700

    Update .gitconfig

--- script/git/branch-list-recent.sh
+++ script/git/branch-list-recent.sh
@@ -6,5 +6,4 @@ git reflog |
   rg -io "moving from ([^\s]+)" |
   awk '{ print $3 }' |
   awk '!a[$0]++' |
-  rg -Fxf <(git branch | cut -c 3-) | # filter by local branches
-  fzf --no-sort
+  rg --color never -Fxf <(git branch | cut -c 3-) # filter by local branches
---
+++ script/git/check-clean-push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu -o pipefail
+
+if [[ -z $(git branch --remotes --contains $(git rph)) ]]; then
+  echo "Branch is not pushed to remote"
+  exit 1
+fi
+if [[ -n $(git status --porcelain) ]]; then
+  echo "Uncommitted changes"
+  exit 1
+fi

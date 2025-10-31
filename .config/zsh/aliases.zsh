11d4d5942329c417ac1c48be83f223bf3184099d
Stefan Toubia <stefan.toubia@gmail.com>
2025-10-31 16:34:22 -0700

    Add some more tools and aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -6,14 +6,20 @@ alias zshrc="code $(readlink ~/.zshrc)"
 alias lg="lazygit"
 
 # File operations
+alias ls=eza
 alias la='ls -la'
 alias ll='ls -l'
 alias lr='ls -R'
+alias lt='ls -T'
+alias ...='../..'
+alias ....='../../..'
+alias .....='../../../..'
 
 # Utility aliases
 alias aliasf="alias | fzf"
 alias cat='bat --paging=never --plain'
 alias batj="bat --language json"
+alias batm="bat --language man"
 alias copyprint="tee >(pbcopy)"
 alias copyprintr="tee >(tr -d '\\n' | pbcopy)"
 alias rgc="rg --color=always --heading --line-number"
